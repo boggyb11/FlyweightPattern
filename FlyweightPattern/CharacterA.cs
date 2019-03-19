f3f08c524a8d1bbcd6a05204100e42e7197b46f2
Benjamin Fitzpatrick <Benjamin_Fitzpatrick@allfleetservices.co.uk>
2019-03-19 14:15:44 +0000

    123

---
+++ FlyweightPattern/CharacterA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyweightPattern
+{
+    /// <summary>
+    /// A concrete flyweight class
+    /// </summary>
+    class CharacterA : Character
+    {
+        public CharacterA()
+        {
+            this.symbol = 'A';
+            this.height = 100;
+            this.width = 120;
+            this.ascent = 70;
+            this.descent = 0;
+        }
+
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol +
+              " (pointsize " + this.pointSize + ")");
+        }
+    }
+}
